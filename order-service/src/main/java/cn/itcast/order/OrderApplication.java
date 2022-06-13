b2946f1a0f657a46e724859efb5729c52c726ebd
daweizi <hello@daweizi.cn>
2022-06-13 15:50:31 +0800

    feign

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,29 +1,34 @@
 package cn.itcast.order;
 
 
+import com.itcast.feign.client.UserClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
+@EnableFeignClients(clients = UserClient.class) //开始feign客户端
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
 
-    @Bean
+
+
+   /* @Bean
     @LoadBalanced
-    /**
+    *//**
      * 创建RestTemplate并注入Spring容器
-     */
+     *//*
     public RestTemplate restTemplate(){
         return new RestTemplate();
-    }
+    }*/
 
 
    /* @Bean
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,27 +2,31 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
+import com.itcast.feign.client.UserClient;
+import com.itcast.feign.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+    //@Autowired
+    //private RestTemplate restTemplate;
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         //2 根据对象查询除User的id
         Long userId = order.getUserId();
-        //3 使用RestTemplate发送请求去找UserService查询User的对象
-        String url = "http://userServer/user/"+userId;
-        User user = restTemplate.getForObject(url,User.class);
+//        //3 使用RestTemplate发送请求去找UserService查询User的对象
+//        String url = "http://userServer/user/"+userId;
+//        User user = restTemplate.getForObject(url,User.class);
+        //使用feign发送请求
+        User user = userClient.findById(userId);
         // 4.返回
         order.setUser(user);
         return order;
